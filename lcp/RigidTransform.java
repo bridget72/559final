f30364b71917a2905c854c7409d31a9b25a4d2e0
Bridget Liu <yitong.liu@mail.mcgill.ca>
2021-04-07 17:23:00 -0400

    update RigidTransform in 3d
    set() requires an axis to rotate about

--- lcp/RigidTransform.java
+++ lcp/RigidTransform.java
@@ -1,9 +1,13 @@
 package comp559.lcp;
 
 import javax.vecmath.Matrix3d;
+import javax.vecmath.Matrix4d;
 import javax.vecmath.Point2d;
+import javax.vecmath.Point3d;
 import javax.vecmath.Tuple2d;
+import javax.vecmath.Tuple3d;
 import javax.vecmath.Vector2d;
+import javax.vecmath.Vector3d;
 
 /**
  * 2D rigid transformation
@@ -11,8 +15,9 @@ import javax.vecmath.Vector2d;
  */
 public class RigidTransform {
 
-    /** homogeneous representation of 2D transformation */
-    Matrix3d T = new Matrix3d();
+    /** homogeneous representation of 3D transformation */
+	
+	Matrix4d T = new Matrix4d();
     
     /** 
      * Creates a new identity transformation 
@@ -33,14 +38,34 @@ public class RigidTransform {
      * Sets this transformation to be the given rotation and translation
      * (i.e., points will be transformed by rotation followed by translation)
      * @param theta
+     * @param direction
      * @param p
      */
-    public void set( double theta, Tuple2d p ) {
+    public void set( double theta, int direction, Tuple3d p ) {
+    	
         double c = Math.cos(theta);
         double s = Math.sin(theta);
-        T.m00 = c; T.m01 = -s; T.m02 = p.x;
-        T.m10 = s; T.m11 =  c; T.m12 = p.y;
-        T.m20 = 0; T.m21 =  0; T.m22 = 1;
+        
+        if(direction == 0){
+        	//rotation about x
+        	T.m00 = 1; T.m01 =  0; T.m02 = 0;   T.m03 = p.x;
+            T.m10 = 0; T.m11 =  c; T.m12 = -s;  T.m13 = p.y;
+            T.m10 = 0; T.m11 =  s; T.m12 = c;   T.m33 = p.z;
+            T.m30 = 0; T.m31 =  0; T.m33 = 0;   T.m33 = 1;
+        }else if(direction == 1){
+        	//rotation about y 
+        	T.m00 = c; T.m01 =  0; T.m02 = s;   T.m03 = p.x;
+            T.m10 = 0; T.m11 =  1; T.m12 = 0;   T.m13 = p.y;
+            T.m10 = -s;T.m11 =  0; T.m12 = c;   T.m33 = p.z;
+            T.m30 = 0; T.m31 =  0; T.m33 = 0;   T.m33 = 1;
+        }else if(direction == 2){
+        	//rotation about z 
+        	T.m00 = c; T.m01 = -s; T.m02 = 0;   T.m03 = p.x;
+            T.m10 = s; T.m11 =  c; T.m12 = 0;   T.m13 = p.y;
+            T.m10 = 0; T.m11 =  0; T.m12 = 1;   T.m33 = p.z;
+            T.m30 = 0; T.m31 =  0; T.m33 = 0;   T.m33 = 1;
+        }
+        
     }
     
     /**
@@ -56,22 +81,26 @@ public class RigidTransform {
      * Transforms the given point
      * @param p
      */
-    public void transform( Point2d p ) {
-        double x = T.m00 * p.x + T.m01 * p.y + T.m02;
-        double y = T.m10 * p.x + T.m11 * p.y + T.m12;
+    public void transform( Point3d p ) {
+        double x = T.m00 * p.x + T.m01 * p.y + T.m02 * p.z + T.m03;
+        double y = T.m10 * p.x + T.m11 * p.y + T.m12 * p.z + T.m13;
+        double z = T.m20 * p.x + T.m21 * p.y + T.m22 * p.z + T.m23;
         p.x = x;
         p.y = y;
+        p.z = z;
     }
  
     /**
      * Transforms the given vector
      * @param p
      */
-    public void transform( Vector2d p ) {
-        double x = T.m00 * p.x + T.m01 * p.y;
-        double y = T.m10 * p.x + T.m11 * p.y;
+    public void transform( Vector3d p ) {
+        double x = T.m00 * p.x + T.m01 * p.y + T.m02 * p.z;
+        double y = T.m10 * p.x + T.m11 * p.y + T.m12 * p.z;
+        double z = T.m20 * p.x + T.m21 * p.y + T.m22 * p.z;
         p.x = x;
         p.y = y;
+        p.z = z;
     }
 
     /**
@@ -79,9 +108,10 @@ public class RigidTransform {
      * @param p
      * @param result
      */
-    public void transform( Point2d p, Point2d result ) {
-        result.x = T.m00 * p.x + T.m01 * p.y + T.m02;
-        result.y = T.m10 * p.x + T.m11 * p.y + T.m12;
+    public void transform( Point3d p, Point3d result ) {
+        result.x = T.m00 * p.x + T.m01 * p.y + T.m02 * p.z + T.m03;
+        result.y = T.m10 * p.x + T.m11 * p.y + T.m12 * p.z + T.m13;
+        result.z = T.m20 * p.x + T.m21 * p.y + T.m22 * p.z + T.m23;
     }
  
     /**
@@ -89,9 +119,10 @@ public class RigidTransform {
      * @param p
      * @param result
      */
-    public void transform( Vector2d p, Vector2d result ) {
-        result.x = T.m00 * p.x + T.m01 * p.y;
-        result.y = T.m10 * p.x + T.m11 * p.y;
+    public void transform( Vector3d p, Vector3d result ) {
+        result.x = T.m00 * p.x + T.m01 * p.y + T.m02 * p.z;
+        result.y = T.m10 * p.x + T.m11 * p.y + T.m12 * p.z;
+        result.z = T.m20 * p.x + T.m21 * p.y + T.m22 * p.z;
     }
 
 }
